1d2de09737f2c04ae792551f4a3a28d905cd0038
Abhinandh B G <abhinandh.b.g@ibm.com>
2024-05-21 20:02:23 +0530

    changes for subcommands

--- cmd/root.go
+++ cmd/root.go
@@ -1,36 +1,43 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
+	"node-describe/constants"
+
+	// _"node-describe/constants"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "node-describe",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+func NewCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "node-describe",
+		Short: "node details in a user friendly manner",
+		Long: `node details in kubectl, wont allow us to get details like, how much is left and all
+	we will use this tool to get those infos`,
+		// Uncomment the following line if your bare application
+		// has an action associated with it:
+		Run: func(cmd *cobra.Command, args []string) {
+			jokeTerm, _ := cmd.Flags().GetString("config")
+			constants.SetCfgFile(jokeTerm)
+			term := constants.GetCfgFile()
+			fmt.Println("testing this ", term)
+		},
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	NewCmd().AddCommand(getCmd)
+
+	// fmt.Println(rootCmd.Flags().GetString("config"))
+	err := NewCmd().Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -41,11 +48,9 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.node-describe.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	NewCmd().Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	NewCmd().PersistentFlags().String("config", "", "config file (default is $HOME/.node-describe.yaml)")
+
 }
-
-
---
+++ internal/kubeclient/kubeclient.go
@@ -0,0 +1,39 @@
+package kubeclient
+
+import (
+	"node-describe/constants"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type Kubeclient struct {
+}
+
+func NewClient() *Kubeclient {
+	return &Kubeclient{}
+}
+
+func getKubeConfig() string {
+	term := constants.GetCfgFile()
+	return term
+}
+
+func (k *Kubeclient) CreateClientObject() *kubernetes.Clientset {
+
+	kubeconfig := getKubeConfig()
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return clientset
+
+}
